8b458e99bd455b926de4f6f6d800390375751ad1
cisco <sovisrushain@gmail.com>
2021-10-21 18:52:33 +0530

    Implement doPost method in auth servlet

--- src/main/java/lk/dep/cisco/authbackend/api/AuthServlet.java
+++ src/main/java/lk/dep/cisco/authbackend/api/AuthServlet.java
@@ -1,12 +1,50 @@
 package lk.dep.cisco.authbackend.api;
 
+import jakarta.json.bind.JsonbBuilder;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import lk.dep.cisco.authbackend.dto.UserDTO;
+import lk.dep.cisco.authbackend.security.SecurityContext;
+import lk.dep.cisco.authbackend.service.UserService;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
-@WebServlet(name = "AuthServlet", value = "/AuthServlet")
+@WebServlet(name = "AuthServlet", value = "/authenticate")
 public class AuthServlet extends HttpServlet {
 
+    @Resource(name = "java:comp/env/jdbc/cp")
+    private DataSource dataSource;
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getContentType() == null || !req.getContentType().startsWith("application/x-www-form-urlencoded")){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid request (Not a valid content-type)");
+            return;
+        }
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        if (username == null || password == null){
+            resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Bad login credentials");
+            return;
+        }
+        try (Connection connection = dataSource.getConnection()) {
+            UserService userService = new UserService(connection);
+            try {
+                UserDTO user = userService.authenticate(username, password);
+                SecurityContext.setPrincipal(user);
+                resp.setContentType("application/json");
+                resp.getWriter().println(JsonbBuilder.create().toJson(user));
+            }catch (RuntimeException e){
+                resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Bad login credentials");
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 }
