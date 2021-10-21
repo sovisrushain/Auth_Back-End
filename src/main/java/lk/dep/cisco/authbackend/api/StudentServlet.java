4312b62c19c42f40f07bad84b0d747dddeb9120c
cisco <sovisrushain@gmail.com>
2021-10-21 17:51:20 +0530

    Add a connection pool to the project

--- src/main/java/lk/dep/cisco/authbackend/api/StudentServlet.java
+++ src/main/java/lk/dep/cisco/authbackend/api/StudentServlet.java
@@ -4,10 +4,19 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+import javax.annotation.Resource;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet(name = "StudentServlet", value = "/students", loadOnStartup = 0)
 public class StudentServlet extends HttpServlet {
+
+    @Resource(name = "java:comp/env/jdbc/cp")
+    private DataSource dataSource;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("doGet");
