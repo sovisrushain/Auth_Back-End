4cc6768af106c0ab040fc49e180f8c89f2aab4b9
cisco <sovisrushain@gmail.com>
2021-10-21 18:48:54 +0530

    Implement doPost method in User service

---
+++ src/main/java/lk/dep/cisco/authbackend/api/AuthServlet.java
@@ -0,0 +1,12 @@
+package lk.dep.cisco.authbackend.api;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "AuthServlet", value = "/AuthServlet")
+public class AuthServlet extends HttpServlet {
+
+}
--- src/main/java/lk/dep/cisco/authbackend/api/UserServlet.java
+++ src/main/java/lk/dep/cisco/authbackend/api/UserServlet.java
@@ -1,20 +1,53 @@
 package lk.dep.cisco.authbackend.api;
 
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.json.bind.JsonbException;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import lk.dep.cisco.authbackend.dto.UserDTO;
+import lk.dep.cisco.authbackend.service.UserService;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @WebServlet(name = "UserServlet", value = "/users", loadOnStartup = 0)
 public class UserServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("doGet");
-    }
+
+    @Resource(name = "java:comp/env/jdbc/cp")
+    private DataSource dataSource;
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("doPost");
+        if (req.getContentType() == null || !req.getContentType().startsWith("application/json")) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid Request (Only support JSON)");
+            return;
+        }
+        try {
+            UserDTO userDTO = JsonbBuilder.create().fromJson(req.getReader(), UserDTO.class);
+            if (userDTO.getUsername() == null || userDTO.getUsername().trim().length() < 3) {
+                resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid username");
+                return;
+            } else if (userDTO.getFullName() == null || !userDTO.getFullName().trim().matches("^[A-Za-z ]+$")) {
+                resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid full name");
+                return;
+            } else if (userDTO.getPassword() == null || userDTO.getPassword().trim().isEmpty()) {
+                resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid password");
+                return;
+            }
+            try (Connection connection = dataSource.getConnection()) {
+                UserService userService = new UserService(connection);
+                userService.saveUser(userDTO);
+                resp.setStatus(HttpServletResponse.SC_CREATED);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        } catch (JsonbException e) {
+            e.printStackTrace();
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid JSON");
+        }
     }
 }
